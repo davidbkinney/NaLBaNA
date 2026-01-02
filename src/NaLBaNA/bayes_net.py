765fcfae046e96b1a5afe5dad0d453b248e34a41
davidbkinney <58792490+davidbkinney@users.noreply.github.com>
2026-01-02 09:44:47 -0500

    Fix conditional probability handling in bayes_net.py

--- src/NaLBaNA/bayes_net.py
+++ src/NaLBaNA/bayes_net.py
@@ -136,6 +136,8 @@ def get_joint_distribution(bayes_net:BayesNet,intervention=None) -> pd.DataFrame
                     probs.append(1.0 if matching[0]['value'] == row[var] else 0.0)
                 else:
                     probs.append(parent_value_match['conditional_probability'])
+            else:
+                    probs.append(parent_value_match['conditional_probability'])
             
         #Obtain the joint probability by finding the product of all the conditional
         #probabilities for children, given their parents.
