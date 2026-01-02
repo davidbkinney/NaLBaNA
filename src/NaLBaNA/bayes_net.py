769fb8bf7297952ce543735fbf2300748b61781a
davidbkinney <58792490+davidbkinney@users.noreply.github.com>
2026-01-02 10:15:34 -0500

    Implement validation for Bayesian Network variables
    Added validation checks for event, condition, and intervention variables in the Bayesian Network.

--- src/NaLBaNA/bayes_net.py
+++ src/NaLBaNA/bayes_net.py
@@ -159,6 +159,21 @@ def get_conditional_probability_table(bayes_net:BayesNet, event_variable:str, co
     Returns:
         A pandas DataFrame representing the conditional probability table.
     """
+    if event_variable not in bayes_net.vars:
+        return f"Inputted event variable {event_variable} is not a node in the Bayesian Network!"
+        break
+    for var in condition_variables:
+        if var not in bayes_net.vars:
+            return f"Inputted condition variable {var} is not a node in the Bayesian Network!"
+            break
+    for var in [i["variable"] for i in intervention]:
+        if var not in bayes_net.vars:
+            return f"Inputted intervention variable {var} is not a node in the Bayesian Network!"
+        intervention_value = [i for i in intervention if i["variable"] == var][0]["value"]
+        variable_values = [val for val in bn.values if i["variable"] = var][0]["values"]
+        if intervention_value not in variable_values:
+            return f"{intervention_value} is not a value of {var} in the Bayesian Network!"
+            break
     joint_df = get_joint_distribution(bayes_net, intervention)
     column_names = [' ']
     condition_values = [v for v in bayes_net.values if v['variable'] in condition_variables]
