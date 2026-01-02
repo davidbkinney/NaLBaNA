7430dae4ce8352b964ef1e81036090094b08eae1
davidbkinney <58792490+davidbkinney@users.noreply.github.com>
2026-01-02 10:18:49 -0500

    Clean up validation logic in bayes_net.py
    Removed unnecessary break statements in validation checks.

--- src/NaLBaNA/bayes_net.py
+++ src/NaLBaNA/bayes_net.py
@@ -161,11 +161,9 @@ def get_conditional_probability_table(bayes_net:BayesNet, event_variable:str, co
     """
     if event_variable not in bayes_net.vars:
         return f"Inputted event variable {event_variable} is not a node in the Bayesian Network!"
-        break
     for var in condition_variables:
         if var not in bayes_net.vars:
             return f"Inputted condition variable {var} is not a node in the Bayesian Network!"
-            break
     for var in [i["variable"] for i in intervention]:
         if var not in bayes_net.vars:
             return f"Inputted intervention variable {var} is not a node in the Bayesian Network!"
@@ -173,7 +171,6 @@ def get_conditional_probability_table(bayes_net:BayesNet, event_variable:str, co
         variable_values = [val for val in bn.values if i["variable"] == var][0]["values"]
         if intervention_value not in variable_values:
             return f"{intervention_value} is not a value of {var} in the Bayesian Network!"
-            break
     joint_df = get_joint_distribution(bayes_net, intervention)
     column_names = [' ']
     condition_values = [v for v in bayes_net.values if v['variable'] in condition_variables]
